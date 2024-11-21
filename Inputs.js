6fefd077b8f8510ee21ff44f6ce4241647d23b94
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-11-21 10:07:09 -0500

    Add files via upload

---
+++ Inputs.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",addlistener); 
+var Number_of_things;
+function addlistener()
+{
+	document.getElementById("Output").style.display = "none";
+	document.getElementById("btn1").addEventListener("click",Getelements);
+	document.getElementById("btn2").addEventListener("click",GetThings);
+
+}
+
+function Getelements()
+{
+	
+}
