726a2b3c8bb6430a28704120cee352be0ca72062
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-04-10 10:19:34 -0400

    Update login.py

--- Project_Supermarket_Web/login.py
+++ Project_Supermarket_Web/login.py
@@ -1,23 +1,20 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect,url_for
 import os.path
 from os import path
 
-fileDir = os.path.dirname(os.path.realpath("__file__"))
-filename = "Ryan" + ".doc"
-file = open(filename, "x")
-file.write("Ryan" + "\n" + "Zhang")
-file.close()
-
 app = Flask(__name__)
 @app.route('/', methods = ["GET","POST"])
+
 def main():
+    print("main")
     if request.method == "GET":
         return render_template("login.html")
     else:
         return render_template("login.html")
         check()
-@app.route("/home",methods = ["GET","POST"])
+@app.route("/",methods = ["GET","POST"])
 def check():
+    print("check")
     fileDir = os.path.dirname(os.path.realpath("__file__"))
     
     username  = request.form.get("username")
@@ -42,22 +39,42 @@ def check():
             print("wrong")
             return render_template("login.html")
 
-@app.route("/signup",methods = ["GET","POST"])
+@app.route("/signup",methods = ["GET"])
 def signup():
+    print("Signup")
     if request.method == "GET":
         return render_template("signup.html")
     else:
-        return render_template("signup.html")
-        create()
-def create():
+        return render_template("home.html")
+        home()
+@app.route('/home',methods = ["GET","POST"])
+def home():
+    print("hello")
     fileDir = os.path.dirname(os.path.realpath("__file__"))
     username = request.form.get("username")
     password = request.form.get("password")
     email = request.form.get("email")
-    filename = username + ".doc"
+    filename = str(username) + ".doc"
+    fileexist = bool(path.exists(filename))
+    print(username, password, email)
+    if (username == "" or password == "" or email == ""):
+        return render_template("signup.html")
+    else:
+        check = len(password)
+        if (check < 6 ):
+            return render_template ("signup.html")
+        else:
+            
+            if (fileexist == False):
+                admin = open(filename, "x")
+                admin.write(username + "\n" + password + "\n" + email)
+                admin.close()
+                print("checkinput")
+            else:
+                return redirect(url_for('home'))
+            
+        return redirect(url_for('home'))
 
 
-
-    
 if __name__ == "__main__":
     app.run()
